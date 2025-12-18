6f311e27abe8b760fd820f26168bfc31279747be
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-12-18 14:45:23 +0100

    Very important

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Important from "./important";
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"],
@@ -19,7 +19,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				<Important />
+				{children}
+			</body>
 		</html>
 	);
 }
