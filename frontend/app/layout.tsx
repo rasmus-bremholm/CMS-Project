3f0020a7c4fed9eb53f8b4aaea088dee0ba6155f
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-01-07 14:30:49 +0100

    Added Navbar to Layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Important from "./important";
+import Navbar from "./components/Navbar";
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"],
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
 		<html lang='en'>
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>
 				<Important />
+				<Navbar />
 				{children}
 			</body>
 		</html>
