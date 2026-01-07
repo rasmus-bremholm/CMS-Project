f769be393352682457b2e9cab6d06a7b739dbec9
patrikboyton <hello@patrikboyton.com>
2026-01-07 18:12:16 +0100

    Added mock product

--- frontend/app/product-details/page.tsx
+++ frontend/app/product-details/page.tsx
@@ -4,6 +4,13 @@ import Image from "next/image";
 import { Box, Container, Grid, Stack, Typography } from "@mui/material";
 
 export default function ProductDetails() {
+	const product = {
+		title: 'Arvid Nordquist, Classic Mellan',
+		price: 89,
+		description: 'Elegant smak med nötiga toner. Lång och frisk eftersmak. Mellan är certifierat av Rainforest Alliance.',
+		inStock: true
+	}
+
   return (
     <Container
       component="main"
@@ -22,17 +29,18 @@ export default function ProductDetails() {
         <Grid size={{ xs: 12, md: 6 }}>
           <Stack sx={{ color: "white" }}>
             <Typography component="h1" variant="h4" fontWeight="bold">
-              Produkttitel
+              {product.title}
             </Typography>
 
-            <Typography>100:-</Typography>
+            <Typography>{product.price}:-</Typography>
 
             <Typography>
-              Description description description description description
-              description description description.
+              {product.description}
             </Typography>
 
-            <Typography>I lager</Typography>
+            <Typography>
+							{product.inStock ? 'I lager' : 'Ej i lager'}
+						</Typography>
           </Stack>
         </Grid>
       </Grid>
