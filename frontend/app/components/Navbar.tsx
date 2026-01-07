ce6c6d3f3e7c4089fbf282215d569b46a71bfaca
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-01-07 14:00:52 +0100

    Navbar first draft

---
+++ frontend/app/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import { Box, IconButton } from "@mui/material";
+import Link from "next/link";
+import Image from "next/image";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+export default function Navbar() {
+	const menuLinks = [
+		{ id: 1, title: "Nyheter", url: "/news" },
+		{ id: 2, title: "Erbjudanden", url: "/discounts" },
+		{ id: 3, title: "Te", url: "/tea" },
+		{ id: 4, title: "Kaffe", url: "/coffee" },
+		{ id: 5, title: "Övrigt", url: "/categories" },
+	];
+
+	return (
+		<Box component='nav'>
+			<Box>
+				<Image src='/logo.png' height={50} width={50} alt='Coffee Time Logo' />
+			</Box>
+			<Box>
+				{menuLinks.map((item) => (
+					<Link key={item.id} href={item.url}>
+						{item.title}
+					</Link>
+				))}
+			</Box>
+			<Box>
+				<IconButton LinkComponent={Link} href='/account'>
+					<AccountCircleIcon />
+				</IconButton>
+				<IconButton LinkComponent={Link} href='/cart'>
+					<ShoppingCartIcon />
+				</IconButton>
+			</Box>
+		</Box>
+	);
+}
