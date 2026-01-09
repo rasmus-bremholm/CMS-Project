aa0d7068b9eef8972aa0e153fddc3fe6dfb153f6
patrikboyton <hello@patrikboyton.com>
2026-01-09 13:41:28 +0100

    Added a brand palette with interface for theme

--- frontend/app/lib/theme/theme.ts
+++ frontend/app/lib/theme/theme.ts
@@ -2,14 +2,47 @@
 
 import { createTheme } from "@mui/material/styles";
 
+declare module '@mui/material/styles' {
+  interface Palette {
+    brand: {
+			whiteSmoke: string,
+			silver: string,
+			coffeeBean: string,
+			darkCoffee: string,
+			carbonBlack: string
+		};
+  }
+  interface PaletteOptions {
+    brand?: {
+			whiteSmoke: string,
+			silver: string,
+			coffeeBean: string,
+			darkCoffee: string,
+			carbonBlack: string
+		};
+  }
+}
+
 export const theme = createTheme({
 	palette: {
+		brand: {
+			whiteSmoke: "#f5f5f5",
+			silver: "#c4bbaf",
+			coffeeBean: "#7f5539",
+			darkCoffee: "#432818",
+			carbonBlack: "#222323"
+		},
 		primary: {
 			main: "#1a1a1a",
 		},
 		background: {
 			default: "#fafafa",
 		},
+		text: {
+      primary: "#222323",
+      secondary: "#432818",
+			disabled: "#4a4a4aff"
+    },
 	},
 	typography: {
 		fontFamily: "inherit",
