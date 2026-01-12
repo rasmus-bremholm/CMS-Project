e4c76c2b6f1900dcdaf524c71c0ff8a42620097f
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-01-12 10:11:25 +0100

    Reorganized

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Important from "./important";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import ThemeRegistry from "./lib/theme/ThemeRegistry";
 
 const geistSans = Geist({
