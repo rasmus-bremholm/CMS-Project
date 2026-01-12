a9b8434eb717ea91c296d8f634e659803e993909
patrikboyton <hello@patrikboyton.com>
2026-01-12 13:18:30 +0100

    Added prev/next buttons for ImageCarousel

--- frontend/app/components/ImageCarousel.tsx
+++ frontend/app/components/ImageCarousel.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import useEmblaCarousel from "embla-carousel-react";
-import { Box } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
+import { ChevronLeft, ChevronRight } from "@mui/icons-material";
 import Image from "next/image";
 
 //Types
@@ -15,7 +16,11 @@ export default function ImageCarousel({ images }: ImageCarouselProps) {
   const [emblaRef] = useEmblaCarousel({ loop: true });
 
   return (
-    <Box sx={{ overflow: "hidden", width: "100%" }} ref={emblaRef}>
+    <Box
+      sx={{ overflow: "hidden", position: "relative", width: "100%" }}
+      ref={emblaRef}
+    >
+      {/* Viewport */}
       <Box sx={{ display: "flex" }}>
         {images.map((img) => (
           <Box
@@ -36,6 +41,37 @@ export default function ImageCarousel({ images }: ImageCarouselProps) {
           </Box>
         ))}
       </Box>
+
+      {/* Prev/Next buttons */}
+      <IconButton
+        sx={{
+          backgroundColor: "rgba(0,0,0,0.3)",
+          color: "rgba(255,255,255,0.9)",
+          left: "1em",
+          position: "absolute",
+          top: "50%",
+          transform: "translateY(-50%)",
+          zIndex: 2,
+          "&:hover": { backgroundColor: "rgba(0,0,0,0.5)" },
+        }}
+      >
+        <ChevronLeft />
+      </IconButton>
+
+      <IconButton
+        sx={{
+          backgroundColor: "rgba(0,0,0,0.3)",
+          color: "rgba(255,255,255,0.9)",
+          right: "1em",
+          position: "absolute",
+          top: "50%",
+          transform: "translateY(-50%)",
+          zIndex: 2,
+          "&:hover": { backgroundColor: "rgba(0,0,0,0.5)" },
+        }}
+      >
+        <ChevronRight />
+      </IconButton>
     </Box>
   );
 }
