5e121b30e85811bfccd2290a5254e74da01c325d
patrikboyton <hello@patrikboyton.com>
2026-01-12 11:37:30 +0100

    Added ImageCarousel.tsx with import och boilerplate

---
+++ frontend/app/components/ImageCarousel.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import useEmblaCarousel from "embla-carousel-react";
+import { Box } from "@mui/material";
+import Image from "next/image";
+
+export default function ImageCarousel(images) {
+  const [emblaRef] = useEmblaCarousel({ loop: true });
+
+  return (
+    <Box sx={{ overflow: "hidden", width: "100%" }} ref={emblaRef}>
+      <Box sx={{ display: "flex" }}>
+        {images.map((img) => (
+          <Box
+            key={img.id}
+            sx={{
+              flex: "0 0 100%",
+              minWidth: 0,
+              position: "relative",
+              height: "400px",
+            }}
+          >
+            <Image
+              src={img.url}
+              alt={img.alt}
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  );
+}
