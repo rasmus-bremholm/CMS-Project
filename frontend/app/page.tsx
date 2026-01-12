3c64537ba0c49555be10f2d7ec71676f7fcbbd21
patrikboyton <hello@patrikboyton.com>
2026-01-12 12:20:28 +0100

    Start on Index.tsx, added test images for ImageCarousel

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,21 @@
+import ImageCarousel from "./components/ImageCarousel";
+
 export default function Home() {
-	return <div></div>;
+	const images = [
+		{
+			id: 1,
+			url: "/1.jpg",
+			alt: "Such access"
+		},
+		{
+			id: 2,
+			url: "/2.jpg",
+			alt: "Much wow"
+		}
+	]
+
+	return (
+		<ImageCarousel images={images}/>
+	);
 }
+
