876357510ffaa49ba01fbaf505eeeb0d5bb68d13
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-01-13 11:12:28 +0100

    css fixes for navbar

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Important from "./important";
 import Navbar from "./components/Navbar";
@@ -6,30 +6,32 @@ import Footer from "./components/Footer";
 import ThemeRegistry from "./lib/theme/ThemeRegistry";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
+const inter = Inter({ subsets: ["latin"] });
+
 export const metadata = {
-	title: "Coffee Time",
-	description: "The only place for Tea & Coffee blends",
+  title: "Coffee Time",
+  description: "The only place for Tea & Coffee blends",
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang='en'>
-			<body className={`${geistSans.variable} ${geistMono.variable}`}>
-				<ThemeRegistry>
-					<Navbar />
-					{children}
-					<Footer />
-				</ThemeRegistry>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeRegistry>
+          <Navbar />
+          {children}
+          <Footer />
+        </ThemeRegistry>
+      </body>
+    </html>
+  );
 }
--- frontend/app/lib/theme/theme.ts
+++ frontend/app/lib/theme/theme.ts
@@ -2,49 +2,67 @@
 
 import { createTheme } from "@mui/material/styles";
 
-declare module '@mui/material/styles' {
+declare module "@mui/material/styles" {
   interface Palette {
     brand: {
-			whiteSmoke: string,
-			silver: string,
-			coffeeBean: string,
-			darkCoffee: string,
-			carbonBlack: string
-		};
+      whiteSmoke: string;
+      silver: string;
+      coffeeBean: string;
+      darkCoffee: string;
+      carbonBlack: string;
+    };
   }
   interface PaletteOptions {
     brand?: {
-			whiteSmoke: string,
-			silver: string,
-			coffeeBean: string,
-			darkCoffee: string,
-			carbonBlack: string
-		};
+      whiteSmoke: string;
+      silver: string;
+      coffeeBean: string;
+      darkCoffee: string;
+      carbonBlack: string;
+    };
+  }
+  interface TypographyVariants {
+    navLink: React.CSSProperties;
+  }
+  interface TypographyVariantsOptions {
+    navLink?: React.CSSProperties;
+  }
+}
+
+declare module "@mui/material/Typography" {
+  interface TypographyPropsVariantOverrides {
+    navLink: true;
   }
 }
 
 export const theme = createTheme({
-	palette: {
-		brand: {
-			whiteSmoke: "#f5f5f5",
-			silver: "#c4bbaf",
-			coffeeBean: "#7f5539",
-			darkCoffee: "#432818",
-			carbonBlack: "#222323"
-		},
-		primary: {
-			main: "#1a1a1a",
-		},
-		background: {
-			default: "#fafafa",
-		},
-		text: {
+  palette: {
+    brand: {
+      whiteSmoke: "#f5f5f5",
+      silver: "#c4bbaf",
+      coffeeBean: "#7f5539",
+      darkCoffee: "#432818",
+      carbonBlack: "#222323",
+    },
+    primary: {
+      main: "#1a1a1a",
+    },
+    background: {
+      default: "#fafafa",
+    },
+    text: {
       primary: "#222323",
       secondary: "#432818",
-			disabled: "#4a4a4aff"
+      disabled: "#4a4a4aff",
     },
-	},
-	typography: {
-		fontFamily: "inherit",
-	},
+  },
+  typography: {
+    fontFamily: "inherit",
+    navLink: {
+      fontSize: "16px",
+      fontWeight: 400,
+      lineHeight: 1,
+      textTransform: "uppercase",
+    },
+  },
 });
