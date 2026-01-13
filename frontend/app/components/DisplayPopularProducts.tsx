57faea4efbb871a5a2a69a2fee9cdc876495e1bb
patrikboyton <hello@patrikboyton.com>
2026-01-13 12:07:04 +0100

    Styling for popularProducts buttons

--- frontend/app/components/DisplayPopularProducts.tsx
+++ frontend/app/components/DisplayPopularProducts.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Box, Button, Grid } from "@mui/material";
+import { alpha, Box, Button, Grid } from "@mui/material";
 import { PopularProductCard } from "./PopularProductCard";
 
 //types
@@ -18,13 +18,45 @@ export const DisplayPopularProducts = ({
 }: Props) => {
   const [category, setCategory] = useState<"coffee" | "tea">("coffee");
 
-	const activeProducts = category === "coffee" ? coffeeProducts : teaProducts
+  const activeProducts = category === "coffee" ? coffeeProducts : teaProducts;
 
   return (
     <Box>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <Button onClick={() => setCategory("coffee")}>Kaffe</Button>
-        <Button onClick={() => setCategory("tea")}>Te</Button>
+      <Box sx={{ display: "flex", justifyContent: "center", gap: "2em" }}>
+        <Button
+          sx={{
+            backgroundColor:
+              category === "coffee"
+                ? "brand.darkCoffee"
+                : (theme) => alpha(theme.palette.brand.darkCoffee, 0.4),
+            color: "brand.whiteSmoke",
+						px: 3,
+            "&:hover": {
+              backgroundColor: (theme) =>
+                alpha(theme.palette.brand.darkCoffee, 0.75),
+            },
+          }}
+          onClick={() => setCategory("coffee")}
+        >
+          Kaffe
+        </Button>
+        <Button
+          sx={{
+            backgroundColor:
+              category === "tea"
+                ? "brand.darkCoffee"
+                : (theme) => alpha(theme.palette.brand.darkCoffee, 0.4),
+            color: "brand.whiteSmoke",
+						px: 3,
+            "&:hover": {
+              backgroundColor: (theme) =>
+                alpha(theme.palette.brand.darkCoffee, 0.75),
+            },
+          }}
+          onClick={() => setCategory("tea")}
+        >
+          Te
+        </Button>
       </Box>
       <Grid container spacing={3}>
         {activeProducts.map((product) => (
