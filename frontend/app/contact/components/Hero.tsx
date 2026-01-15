a2838b02d3b4f1eaefd3056cf5b0d26b03c1471e
Felicia Bengtsson <bengtssonfelicia@gmail.com>
2026-01-15 09:52:10 +0100

    hero section for cotact page

--- frontend/app/contact/components/Hero.tsx
+++ frontend/app/contact/components/Hero.tsx
@@ -1,11 +1,19 @@
 "use client";
 
-import { Box, Container } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 
 export default function Hero() {
   return (
-    <Box>
-      <Container></Container>
+    <Box sx={{ bgcolor: "beige", py: { xs: 8, md: 12 }, textAlign: "center" }}>
+      <Container maxWidth="md">
+        <Typography variant="h3" fontWeight={600} mb={2}>
+          Get in Touch
+        </Typography>
+        <Typography color="text.secondary">
+          Have a question or just want to say hello? We&apos;d love to hear from
+          you.
+        </Typography>
+      </Container>
     </Box>
   );
 }
