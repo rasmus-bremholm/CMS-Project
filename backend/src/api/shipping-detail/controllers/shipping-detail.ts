7a9ab808c8faafa820bc47851a8961376a400242
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-01-15 11:06:53 +0100

    shippingDetails

---
+++ backend/src/api/shipping-detail/controllers/shipping-detail.ts
@@ -0,0 +1,7 @@
+/**
+ * shipping-detail controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::shipping-detail.shipping-detail');
---
+++ backend/src/api/shipping-detail/routes/shipping-detail.ts
@@ -0,0 +1,7 @@
+/**
+ * shipping-detail router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::shipping-detail.shipping-detail');
