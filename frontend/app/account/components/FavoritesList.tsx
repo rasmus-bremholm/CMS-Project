2ed070b286309cba5b0c2a50d71556cd3a740a12
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-01-19 14:34:09 +0100

    Favorites list
    (not finished)

---
+++ frontend/app/account/components/FavoritesList.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { Box, Typography, Divider } from "@mui/material";
+import { Product } from "@/types/product";
+import { useLanguage } from "@/app/context/LanguageContext";
+import { FavoritesLocale } from "../lib/AccountDashBoard";
+
+interface FavoritesListProps {
+  favorites: Product[];
+}
+
+export default function FavoritesList({ favorites }: FavoritesListProps) {
+  const currentLocale = useLanguage().locale;
+
+  return (
+    <Box sx={{ display: "flex", gap: 2 }}>
+      {favorites ? (
+        favorites.map(favorite => <Box key={favorite.id}></Box>)
+      ) : (
+        <Typography>{FavoritesLocale.missing[currentLocale]}</Typography>
+      )}
+    </Box>
+  );
+}
