3c60505f6edf233de529b4dd2f2b5495162a168b
patrikboyton <hello@patrikboyton.com>
2026-01-20 21:23:07 +0100

    Fixed typing error + formatting

--- frontend/app/product/[slug]/ProductActions.tsx
+++ frontend/app/product/[slug]/ProductActions.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import { useState } from "react";
-import { Stack } from '@mui/material';
+import { Stack } from "@mui/material";
 import { QuantitySelector } from "../../components/QuantitySelector";
 import AddToCartButton from "@/app/components/AddToCartButton";
 
 export const ProductActions = ({ product }: { product: any }) => {
   const [quantity, setQuantity] = useState(1);
 
-  const handleDecrease = () => setQuantity((prev) => (prev > 1 ? prev - 1 : 1));
-  const handleIncrease = () => setQuantity((prev) => prev + 1);
+  const handleDecrease = () => setQuantity(prev => (prev > 1 ? prev - 1 : 1));
+  const handleIncrease = () => setQuantity(prev => prev + 1);
 
   return (
     <Stack direction="row" spacing={2}>
@@ -19,7 +19,7 @@ export const ProductActions = ({ product }: { product: any }) => {
         onIncrease={handleIncrease}
       />
 
-      <AddToCartButton product={product}>Lägg till</AddToCartButton>
+      <AddToCartButton product={product} />
     </Stack>
   );
 };
