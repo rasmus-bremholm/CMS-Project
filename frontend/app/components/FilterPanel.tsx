69ba64f1f350f2586811ecc480b513dfe272b40e
patrikboyton <hello@patrikboyton.com>
2026-01-20 10:47:23 +0100

    FilterPanel function ish-working, will test properly with product gallery

--- frontend/app/components/FilterPanel.tsx
+++ frontend/app/components/FilterPanel.tsx
@@ -1,25 +1,46 @@
 "use client";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { Box, Checkbox, FormControlLabel, Typography } from "@mui/material";
+import { Box, Checkbox, Chip, Divider, FormControlLabel } from "@mui/material";
 
 export default function FilterPanel({ tags }) {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleFilterChange = (tag: string) => {
+    const params = new URLSearchParams(searchParams);
+
+    console.log("params", params);
+    console.log("pathname", pathname);
+    if (params.has("tag", tag)) {
+      params.delete("tag", tag);
+    } else {
+      params.append("tag", tag);
+    }
+
+    router.push(`${pathname}?${params.toString()}`);
+  };
+
   return (
-    <Box sx={{ display: "flex", justifyContent: "center" }}>
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
       <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <Typography>Filter:</Typography>
+        {tags.map(tag => (
+          <FormControlLabel
+            key={tag.id}
+            control={
+              <Checkbox
+                onClick={() => handleFilterChange(tag.title)}
+                sx={{ "& .MuiSvgIcon-root": { fontSize: 16 } }}
+              />
+            }
+            label={tag.title}
+          />
+        ))}
       </Box>
-      {tags.map(tag => (
-        <FormControlLabel
-          key={tag.id}
-          control={
-            <Checkbox
-              sx={{ "& .MuiSvgIcon-root": { fontSize: 16 } }}
-            />
-          }
-          label={tag.title}
-        />
-      ))}
+      <Divider>
+        <Chip label="Filter" size="small" />
+      </Divider>
     </Box>
   );
 }
