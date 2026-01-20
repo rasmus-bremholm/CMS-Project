23b487064096a5b441677ad14c5ea786a3c75a42
patrikboyton <hello@patrikboyton.com>
2026-01-20 22:51:41 +0100

    Remove old code used in mockdata JSON-fetch.

--- frontend/app/home/components/PopularProducts.tsx
+++ frontend/app/home/components/PopularProducts.tsx
@@ -1,33 +1,12 @@
 import { Box, Typography } from "@mui/material";
 import { DisplayPopularProducts } from "./DisplayPopularProducts";
 import { getPopularProducts } from "@/app/lib/utils/strapi";
-//types
-import { Product } from "@/types/product";
-
-//const allProducts = await getAllProducts();
-
-/* const getPopularProducts = (
-  products: Product[],
-  category: "coffee" | "tea"
-) => {
-  return products
-    .filter((p) => p.category === category)
-    .sort((a, b) => b.rating - a.rating)
-    .slice(0, 3);
-}; */
 
 export default async function PopularProducts() {
-  /* const popularCoffee = getPopularProducts(allProducts, "coffee");
-  const popularTea = getPopularProducts(allProducts, "tea"); */
   const [popularCoffee, popularTea] = await Promise.all([
     getPopularProducts("Kaffe"),
     getPopularProducts("Te"),
   ]);
-
-	console.log('popularCoffee', popularCoffee)
-	console.log('popularTea', popularTea)
-	
-	
 
   return (
     <Box
@@ -51,11 +30,3 @@ export default async function PopularProducts() {
     </Box>
   );
 }
-
-/* async function getAllProducts() {
-  const mod = await import("@/app/mockdata/products.json");
-  const products = mod.default;
-
-  return products;
-}
- */
