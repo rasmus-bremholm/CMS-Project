16e3e0daf1d80e303db5af26aa7281549e1b92be
patrikboyton <hello@patrikboyton.com>
2026-01-20 20:55:03 +0100

    Changed name of cataloge product-details to simply product for a more describing URL while navigating between categories and product view

--- frontend/app/categories/[slug]/components/ProductCard.tsx
+++ frontend/app/categories/[slug]/components/ProductCard.tsx
@@ -26,7 +26,7 @@ export const ProductCard = ({ product }: Props) => {
         boxShadow: 3,
       }}
     >
-      <Link href={`/product-details/${product.slug}`}>
+      <Link href={`/product/${product.slug}`}>
         <CardMedia
           component="img"
           height="200"
@@ -42,7 +42,7 @@ export const ProductCard = ({ product }: Props) => {
         <Typography
           variant="h6"
           component={Link}
-          href={`/product-details/${product.slug}`}
+          href={`/product/${product.slug}`}
           sx={{
             color: "inherit",
             fontSize: 18,
